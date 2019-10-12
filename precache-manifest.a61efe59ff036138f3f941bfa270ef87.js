e534be9fc26d609902010550d50b479188180910
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2019-10-12 14:53:05 +0200

    Updates

---
+++ precache-manifest.a61efe59ff036138f3f941bfa270ef87.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cb4b01dcb48132959725f6b58763b9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "75e62e8763db856f8bc5",
+    "url": "/portfolio/static/css/main.c77ee74d.chunk.css"
+  },
+  {
+    "revision": "be72af202b203d58a5fa",
+    "url": "/portfolio/static/js/2.e13c4eb9.chunk.js"
+  },
+  {
+    "revision": "75e62e8763db856f8bc5",
+    "url": "/portfolio/static/js/main.dca0a24d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "8ba35f75e74a482ffc3658b2bf54a5ea",
+    "url": "/portfolio/static/media/all_devices.8ba35f75.jpg"
+  },
+  {
+    "revision": "d952928eaebc0cc1e7b52ca084dacb44",
+    "url": "/portfolio/static/media/all_devices_2.d952928e.jpg"
+  },
+  {
+    "revision": "3cbbe32e3432f416eb20837894dda425",
+    "url": "/portfolio/static/media/avatar.3cbbe32e.jpg"
+  },
+  {
+    "revision": "8e95479da2c5e493b835c8533c2d5ba5",
+    "url": "/portfolio/static/media/css.8e95479d.jpg"
+  },
+  {
+    "revision": "fada9faeeb9aba3a1c5aa6b70e0dbb7b",
+    "url": "/portfolio/static/media/gimp.fada9fae.jpg"
+  },
+  {
+    "revision": "5c560e139fb603958c1e10433cb8108b",
+    "url": "/portfolio/static/media/git.5c560e13.jpg"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/portfolio/static/media/js.0e4fdce8.jpg"
+  },
+  {
+    "revision": "1e38b3c04390ea3f9045e12073b7e269",
+    "url": "/portfolio/static/media/jsx.1e38b3c0.jpg"
+  },
+  {
+    "revision": "26256363ea0ae7f98ddcf60a700951c4",
+    "url": "/portfolio/static/media/learn.26256363.jpg"
+  },
+  {
+    "revision": "9b7ed4aa5100db62e7d7dd21edb55b12",
+    "url": "/portfolio/static/media/linux.9b7ed4aa.jpg"
+  },
+  {
+    "revision": "beb82d40ac8c0c11eb388021e5cc067c",
+    "url": "/portfolio/static/media/m_project.beb82d40.jpg"
+  },
+  {
+    "revision": "a9d7235c9815f9ce738ee3f2c00e82c5",
+    "url": "/portfolio/static/media/m_tablet.a9d7235c.jpg"
+  },
+  {
+    "revision": "7bc86e7e4dc544fa922f174de927bc60",
+    "url": "/portfolio/static/media/mini_me.7bc86e7e.jpg"
+  },
+  {
+    "revision": "444b6794cd48df678934710782884f0f",
+    "url": "/portfolio/static/media/my_page1.444b6794.jpg"
+  },
+  {
+    "revision": "38cf9aff8f5195b76a2a0491d2ccc8eb",
+    "url": "/portfolio/static/media/portfolio.38cf9aff.jpg"
+  },
+  {
+    "revision": "f324ecac728f4d2c103a34068105a53f",
+    "url": "/portfolio/static/media/portfolio2.f324ecac.jpg"
+  },
+  {
+    "revision": "ee21bd7fb144adc6b58ebc1b813dcb1b",
+    "url": "/portfolio/static/media/progr_m.ee21bd7f.jpg"
+  },
+  {
+    "revision": "5c0e774646f6d9f26c650ba963c93f47",
+    "url": "/portfolio/static/media/progr_md.5c0e7746.jpg"
+  },
+  {
+    "revision": "c061e55870f81b1cf0f60c3fdd24a16d",
+    "url": "/portfolio/static/media/react.c061e558.jpg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/portfolio/static/media/reactbootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "1ee1db8ba0f4ae9cec4de9ccc5681f5e",
+    "url": "/portfolio/static/media/sass.1ee1db8b.jpg"
+  },
+  {
+    "revision": "eb59ebfa5e3e768cd773920d9bb44c18",
+    "url": "/portfolio/static/media/v_all_devices.eb59ebfa.jpg"
+  },
+  {
+    "revision": "c604c3a652d2e88eb5c499f241318a71",
+    "url": "/portfolio/static/media/yarn.c604c3a6.jpg"
+  }
+]);
